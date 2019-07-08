12d5a478abb3e8181c975d06be7b8f9f2e97ad22
Viniciusog <vinijipa23@gmail.com>
2019-07-08 14:18:55 -0400

    Operacao de instanciacao de base de dados

---
+++ src/main/java/com/vinicius/workshopmongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.vinicius.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.vinicius.workshopmongo.domain.User;
+import com.vinicius.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		//Id recebe null pois o banco de dados vai gerar este id
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.save(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
