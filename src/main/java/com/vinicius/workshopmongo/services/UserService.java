cfd43664fc73587d293b4703b181e82480c6b704
Viniciusog <vinijipa23@gmail.com>
2019-07-08 18:11:18 -0400

    Delecao de usuario com DELETE

--- src/main/java/com/vinicius/workshopmongo/services/UserService.java
+++ src/main/java/com/vinicius/workshopmongo/services/UserService.java
@@ -37,4 +37,9 @@ public class UserService {
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
+	
+	public void delete(String id) {
+		findById(id); //Fazer uma procura pelo id para ver se existe, se não tiver, vai retornar uma exceção
+		repo.delete(id);
+	}
 }
