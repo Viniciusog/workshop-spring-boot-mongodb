cdeb79d4545697605e72158cd1de13d6d981c484
Viniciusog <vinijipa23@gmail.com>
2019-07-17 22:01:21 -0400

    Consulta simples com query methods

--- src/main/java/com/vinicius/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vinicius/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.vinicius.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,7 @@ import com.vinicius.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	//Só essa declaração faz com que o spring date monte para agente a consulta - Aula 286
+	//IgnoreCase o spring data já entende que é para ignorar letras maiusculas e minusculas
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/vinicius/workshopmongo/services/PostService.java
+++ src/main/java/com/vinicius/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.vinicius.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,9 @@ public class PostService {
 		}
 		return user;
 
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text); 
 	}
 }
