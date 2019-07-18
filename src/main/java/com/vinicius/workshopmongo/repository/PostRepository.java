80977e87964b688ba7bb9e8c4ee6d7bcc4b7f625
Viniciusog <vinijipa23@gmail.com>
2019-07-17 22:09:06 -0400

    Consulta simples com @Query

--- src/main/java/com/vinicius/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/vinicius/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.vinicius.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.vinicius.workshopmongo.domain.Post;
@@ -11,6 +12,9 @@ import com.vinicius.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title'  : { $regex: ?0, $options: 'i' } } ")
+	List<Post> searchTitle(String text);
+	
 	//Só essa declaração faz com que o spring date monte para agente a consulta - Aula 286
 	//IgnoreCase o spring data já entende que é para ignorar letras maiusculas e minusculas
 	List<Post> findByTitleContainingIgnoreCase(String text);
